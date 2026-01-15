91d19722755c18fac57c23533c23cf7c5b1346aa
Anlil Iwas <anliliwas1@gmail.com>
2026-01-14 21:05:51 -0600

    d
    fix1

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/App.css';
 
-const container = document.getElementById('root')!;
-const root = ReactDOM.createRoot(container);
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
